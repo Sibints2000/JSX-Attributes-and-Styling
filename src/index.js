d6ed8b58922ecb2433958c473221c5a06bacd6f7
Sibin t s <62069255+sibin2000@users.noreply.github.com>
2020-12-27 21:46:03 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const img = "https://picsum.photos/200";
+
+ReactDOM.render(
+  <div>
+    <h1 className="heading">My Favourite Foods</h1>
+    <img alt="random" src={img + "?grayscale"} />
+  </div>,
+  document.getElementById("root")
+);
